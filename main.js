b1d3c2eaa89427ffe7a2ab3b011982ceb5f75562
Neeraj <78613839+Chinni566@users.noreply.github.com>
2023-02-19 12:27:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ function setup() {
     video.size(380,380);
     video.hide();
   }
+
+  function start(){
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects" ;
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!")
+  status = true;
+  video.loop();
+  video.speed(1);
+  video.volume(0);
+}
+
+function draw(){
+  image(video,0,0,480,380);
+
+}
